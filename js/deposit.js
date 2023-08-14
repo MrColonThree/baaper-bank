4e248b9d3278b335389ab5aed7053ba6c3782dec
MrColonThree <abdullahalmonir364@gmail.com>
2023-08-15 03:09:57 +0600

    added deposit and withdraw button event

--- js/deposit.js
+++ js/deposit.js
@@ -1,11 +1,29 @@
 // Step-1 : Add event listener to the deposit button
-document.getElementById('btn-deposit').addEventListener('click', function(){
+document.getElementById("btn-deposit").addEventListener("click", function () {
   // Step-2: Get the deposit amount from the deposit field
-  const depositField = document.getElementById('deposit-field');
-  const depositAmount = depositField.value;
+  const depositField = document.getElementById("deposit-field");
+  const newDepositAmountString = depositField.value;
+  const newDepositAmount = parseFloat(newDepositAmountString);
+
   // Step-3: Get the current deposit total
   // For non-input (element other than input textarea) use innerText  to get the text
-  const depositTotalElement = document.getElementById('deposit-total') 
-  const depositTotal = depositTotalElement.innerText;
-  depositTotalElement.innerText = depositAmount; 
-})
+  const depositTotalElement = document.getElementById("deposit-total");
+  const previousDepositTotalString = depositTotalElement.innerText;
+  const previousDepositTotal = parseFloat(previousDepositTotalString);
+  depositTotalElement.innerText = newDepositAmount;
+  
+  // Add number to set the total deposit
+  const currentDepositTotal = previousDepositTotal + newDepositAmount;
+  depositTotalElement.innerText = currentDepositTotal;
+  
+  // Step-5: get ballance current total
+  const balanceTotalElement = document.getElementById('balance-total');
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  // Step-6: calculate current total balance
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  // Set the balance total
+  balanceTotalElement.innerText = currentBalanceTotal;
+  // Step-7: Clear the deposit field
+  depositField.value = "";
+});
